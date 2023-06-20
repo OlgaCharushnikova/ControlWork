ed8e8fc9b1407ef341c48a62677a1082ea383746
Olga Charushnikova <olga_charushnikova@bk.ru>
2023-06-20 20:35:16 +0300

    Добавлен метод создания массива

---
+++ ControlWork/Program.cs
@@ -0,0 +1,8 @@
+﻿void Array(string[] array, int l)
+{
+    for (int i = 0; i < l; i++)
+    {
+        Console.WriteLine("Введите элемент массива: ");
+        array[i] = Console.ReadLine();
+    }
+}
